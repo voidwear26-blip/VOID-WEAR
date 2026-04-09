19fc124e4583621dc374b766dc9a4d58a3f6ad76
Void Wear <voidwear26@gmail.com>
2026-04-09 17:17:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-192137991-85483",
+  "appId": "1:324229749108:web:8726c0e0d06aeace5a7421",
+  "apiKey": "AIzaSyA8JMrO2ec4U7K1KNkVW9Abb7JNB0MOvzw",
+  "authDomain": "studio-192137991-85483.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "324229749108"
+};
